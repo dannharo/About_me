6706c829ca6625e359a266f67c8c6b96add95c3a
Yair <ycosio@ucol.mx>
2017-02-19 23:22:56 -0600

    menu scroll

--- js/general_scripts.js
+++ js/general_scripts.js
@@ -11,4 +11,12 @@ $( function() {
     	$(this).addClass("active");
     });
 
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
   } );
